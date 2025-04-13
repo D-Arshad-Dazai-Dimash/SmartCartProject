ee8daed86058c431930c54930161b5e3eaabab01
color.ui <eskendir16092004@gmail.com>
2025-04-13 19:19:46 +0500

    Just added sign up text

--- app/src/main/java/com/example/myapplication/ui/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/SignUpScreen.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.ui.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -12,19 +13,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,30 +40,66 @@ fun SignUpScreen() {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var confirmPassword by remember { mutableStateOf("") }
-    Box(){
-        Box(
-            modifier = Modifier
-                .background(Color.Gray)
-                .systemBarsPadding()
-                .consumeWindowInsets(WindowInsets.systemBars)
-        ) {
-            PutBackGroundImage(R.drawable.signup)
-        }
-        Column(modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 30.dp)){
-            Column(modifier = Modifier.
-            padding(top = 95.dp).
-            fillMaxWidth()) {
-                Text("Sign Up" , style = TextStyle(fontSize = 42.sp), fontWeight = FontWeight.W700 , color = Color(0xFF363636))
-                Text("Please enter your details" , style = TextStyle(fontSize = 16.sp) , fontWeight = FontWeight.SemiBold )
-            }
-            Column(modifier = Modifier.
-            fillMaxSize().
-            padding(bottom = 50.dp)) {
+    Box(
+        modifier = Modifier
+            .background(Color.Gray)
+            .systemBarsPadding()
+            .consumeWindowInsets(WindowInsets.systemBars)
+    ) {
+        PutBackGroundImage(R.drawable.signup)
 
+        Column(Modifier.fillMaxWidth()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 30.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(top = 95.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        "Sign Up",
+                        style = TextStyle(fontSize = 42.sp),
+                        fontWeight = FontWeight.W700,
+                        color = Color(0xFF363636)
+                    )
+                    Text(
+                        "Please enter your details",
+                        style = TextStyle(fontSize = 16.sp),
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 100.dp)
+                ) {
+                    Button(
+                        onClick = { },
+                        modifier = Modifier.size(354.dp, 52.dp),
+                        border = BorderStroke(1.dp, Color(0xFFD1CFCF)),
+                        colors = ButtonColors(
+                            containerColor = Color.White,
+                            contentColor = Color.Black,
+                            disabledContentColor = Color.Black,
+                            disabledContainerColor = Color.White
+                        )
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.google_button),
+                            contentDescription = "google icon",
+                            modifier = Modifier
+                                .size(15.dp)
+                        )
+                        Text("Sign up with Google" , style = TextStyle(fontSize = 16.sp , fontWeight = FontWeight.Medium), modifier = Modifier.padding(8.dp))
+                    }
+                }
             }
         }
-
     }
+
 }
+

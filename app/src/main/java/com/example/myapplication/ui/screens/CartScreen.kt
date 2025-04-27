58f41d8460c352be5ba4decc70f792111c8ff4af
color.ui <eskendir16092004@gmail.com>
2025-04-27 19:30:39 +0500

    Home Page with bottom navigation bar Without FireBase

---
+++ app/src/main/java/com/example/myapplication/ui/screens/CartScreen.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun CartScreen(navController: NavController) {
+}
